9e5805ec8719bd74ab20d74233d91a09d97fbfe6
Tomer <yodatk@gmail.com>
2020-10-08 02:13:04 +0300

    added MVV_LVA to Moves.kt, still didn't integrate it

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,11 +24,12 @@ Main
 @ExperimentalUnsignedTypes
 fun main() {
     Attacks.initAll()
-    val debug = false
+    val debug = true
     if(debug){
-        val b = Board("rnbqkbnr/ppp1pppp/8/8/3Pp3/2N5/PPP2PPP/R1BQKBNR b KQkq - 1 3" )
-        b.printBoard()
-        Search.searchPosition(b,1)
+//        val b = Board("rnbqkbnr/ppp1pppp/8/8/3Pp3/2N5/PPP2PPP/R1BQKBNR b KQkq - 1 3" )
+//        b.printBoard()
+//        Search.searchPosition(b,1)
+        println("move score: ${Moves.MVV_LVA[Piece.P.ordinal][Piece.n.ordinal]}")
 
     }
     else{
