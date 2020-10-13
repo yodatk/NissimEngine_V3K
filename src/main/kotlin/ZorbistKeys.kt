cf13760c7aba7f148d0c45b30c9fd2f04121734c
Tomer <yodatk@gmail.com>
2020-10-14 00:28:57 +0300

    added hash to Board.kt

--- src/main/kotlin/ZorbistKeys.kt
+++ src/main/kotlin/ZorbistKeys.kt
@@ -7,6 +7,14 @@ import enums.Square
 @ExperimentalUnsignedTypes
 object ZorbistKeys {
 
+    val HASH_FLAG_EXACT = 0
+    val HASH_FLAG_ALPHA = 1
+    val HASH_FLAG_BETA = 2
+
+    val HASH_TABLE_SIZE = 0x400000
+
+    data class TT(var hashKey:ULong,var depth: Int,var flag:Int,var score:Int)
+
     var pieceKeys :Array<Array<ULong>> = Array(12) { Array(64) { 0UL} }
 
     var enpassantKeys :Array<ULong> =  Array(64) { 0UL}
@@ -14,6 +22,18 @@ object ZorbistKeys {
     var castleKeys :Array<ULong> =  Array(16) { 0UL}
 
     var sideKey : ULong = 0UL
+
+    var hashTable:Array<TT> = Array(HASH_TABLE_SIZE) {TT(0UL,0,0,0)}
+
+    fun clearHashTable() {
+        for (tt in hashTable){
+            tt.hashKey = 0UL
+            tt.depth = 0
+            tt.flag = 0
+            tt.score = 0
+        }
+    }
+
 
     fun initRandomKeys() {
         RandomNumbers.uIntRandomState = 1804289383U
